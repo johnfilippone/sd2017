ee1f3f32454e25bd9fb8ed8c602d0a087bc9a68c
Jeff Bell <jeff@jhbell.com>
2017-01-18 22:16:55 -0600

    Add todo items

--- P1/filippone-bell/introspect/Main.java
+++ P1/filippone-bell/introspect/Main.java
@@ -3,16 +3,38 @@ package introspect;
 import java.io.File;
 
 public class Main {
-  public static void main(String[] args) {
-      String packageName = args[0];
-      String pathToPackage = args[1];
 
-      File dir = new File(pathToPackage);
-      File[] fList = dir.listFiles();
-      for (File file : fList){
-          System.out.println(file.getName());
-      }
-      
-  }
+    public static void main(String[] args) {
+        String packageName = args[0];
+        String pathToPackage = args[1];
+
+        // TODO: Open up a new file for writing, write the header info
+        // This could be done in a separate method possibly
+
+        File dir = new File(pathToPackage);
+        File[] fList = dir.listFiles();
+        for (File file : fList) {
+            System.out.println(file.getName());
+            // TODO: IF substring of last 6 characters is .class,
+            // call forName and add info to file -- maybe do this
+            // in separate method, dumpClassData(String className, outfile)
+        }
+        
+    }
+
+    // TODO: Implement a method similar to sudo code below and run for
+    // each class file
+    /*
+    print class info to Prolog file
+    int memberCount = 0;
+    for (Constructor : filename.getDeclaredConstructors()) {
+        print Prolog info
+        increment member count
+    }
+    Do same for fields
+    Do same for methods
+    */
+        
+
 }
 
