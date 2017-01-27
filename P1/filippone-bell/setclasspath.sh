ff0b5b92b4d7a92e3f463bd7835d86c1c57b482e
Jeff Bell <jeff@jhbell.com>
2017-01-26 20:33:44 -0600

    Update setclasspath.sh

--- P1/filippone-bell/setclasspath.sh
+++ P1/filippone-bell/setclasspath.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-CLASSPATH=~/Desktop/Spring_2017/Software_Design/P1/filippone-bell/:~/Desktop/Spring_2017/Software_Design/P1/filippone-bell/tests/lib/hamcrest-core-1.3.jar:~/Desktop/Spring_2017/Software_Design/P1/filippone-bell/tests/lib/junit-4.12.jar:~/Desktop/Spring_2017/Software_Design/P1/filippone-bell/tests/:~/Desktop/Spring_2017/Software_Design/P1/filippone-bell/tests/lib/RegTest.jar:~/Desktop/Spring_2017/Software_Design/P1/filippone-bell/tests/ClassesOdd
-
+CLASSPATH=$PWD:
+CLASSPATH+=$PWD/tests/lib/hamcrest-core-1.3.jar:
+CLASSPATH+=$PWD/tests/lib/junit-4.12.jar:
+CLASSPATH+=$PWD/tests:
+CLASSPATH+=$PWD/tests/lib/RegTest.jar:
+CLASSPATH+=$PWD/tests/ClassesOdd:
 export CLASSPATH
 echo "done"
