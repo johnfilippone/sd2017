8328925e0dae47e05c04f010311391dd27271b9b
Jeff Bell <jeff@jhbell.com>
2017-01-26 21:23:26 -0600

    Use StringBuilder ya fool

--- P1/filippone-bell/introspect/Main.java
+++ P1/filippone-bell/introspect/Main.java
@@ -29,7 +29,11 @@ public class Main {
     public static String getTables(String packageName, String pathToPackage) throws ClassNotFoundException {
         
         // stub code for prolog files to set up tables
-        String output = "dbase(prog1,[bcClass,bcMember]).\n\ntable(bcClass,[cid,\"name\",\"superName\"]).\ntable(bcMember,[mid,cid,static,\"type\",\"sig\"]).\n\n";
+        StringBuilder output = new StringBuilder();
+        // append the basic info
+        output.append("dbase(prog1,[bcClass,bcMember]).\n\n");
+        output.append("table(bcClass,[cid,\"name\",\"superName\"]).\n");
+        output.append("table(bcMember,[mid,cid,static,\"type\",\"sig\"]).\n\n");
 
         // find all class files in package
         File dir = new File(pathToPackage);
@@ -43,15 +47,15 @@ public class Main {
                 
                 // get class
                 Class<?> c = Class.forName(packageName + "." + file.getName().substring(0,file.getName().lastIndexOf('.')));
-                output += String.format("bcClass(c%d,'%s','%s').\n", classID, c.getName(), c.getSuperclass().getSimpleName());
+                output.append(String.format("bcClass(c%d,'%s','%s').\n", classID, c.getName(), c.getSuperclass().getSimpleName()));
                 
                 memberID = 0;
                 // get constructors
                 Constructor<?>[] constructors = c.getDeclaredConstructors();
                 for (Constructor<?> constructor : constructors) {
                     if (Modifier.isPublic(constructor.getModifiers())) {
-                        String signature = getSigniture(constructor.getParameterTypes());
-                        output += String.format("bcMember(m%d,c%d,true,'%s','%s').\n", memberID, classID, c.getName(), c.getSimpleName() + signature);
+                        String signature = getSignature(constructor.getParameterTypes());
+                        output.append(String.format("bcMember(m%d,c%d,true,'%s','%s').\n", memberID, classID, c.getName(), c.getSimpleName() + signature));
                         memberID += 1;
                     }
                 }
@@ -60,7 +64,8 @@ public class Main {
                 Field[] fields = c.getDeclaredFields();
                 for (Field field : fields) {
                     if (Modifier.isPublic(field.getModifiers())) {
-                        output += String.format("bcMember(m%d,c%d,%s,'%s','%s').\n", memberID, classID, Modifier.isStatic(field.getModifiers()), field.getType().getSimpleName(), field.getName());
+                        boolean isStatic = Modifier.isStatic(field.getModifiers());
+                        output.append(String.format("bcMember(m%d,c%d,%s,'%s','%s').\n", memberID, classID, isStatic, field.getType().getSimpleName(), field.getName()));
                         memberID += 1;
                     }
                 }
@@ -69,8 +74,9 @@ public class Main {
                 Method[] methods = c.getDeclaredMethods();
                 for (Method method : methods) {
                     if (Modifier.isPublic(method.getModifiers())) {
-                        String signature = getSigniture(method.getParameterTypes());
-                        output += String.format("bcMember(m%d,c%d,%s,'%s','%s').\n", memberID, classID, Modifier.isStatic(method.getModifiers()), method.getReturnType().getSimpleName(), method.getName() + signature);
+                        String signature = getSignature(method.getParameterTypes());
+                        boolean isStatic = Modifier.isStatic(method.getModifiers());
+                        output.append(String.format("bcMember(m%d,c%d,%s,'%s','%s').\n", memberID, classID, isStatic, method.getReturnType().getSimpleName(), method.getName() + signature));
                         memberID += 1;
                     }
                 }
@@ -79,15 +85,19 @@ public class Main {
             }
         }
 
-        return output;
+        return output.toString();
     }
     
-    private static String getSigniture(Class<?>[] parameters) {
-        String sig = "(";
+    private static String getSignature(Class<?>[] parameters) {
+        StringBuilder signatureBuilder = new StringBuilder();
+        signatureBuilder.append("(");
         for (Class<?> param : parameters) {
-            sig += param.getSimpleName() + ",";
+             signatureBuilder.append(param.getSimpleName());
+             signatureBuilder.append(",");
         }
-        return sig.substring(0,sig.length()-1) + ")";
+        String signature = signatureBuilder.toString();
+        signature = signature.substring(0, signature.length() - 1);
+        return signature + ")";
     }
 
 
