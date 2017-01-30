ae3d8a55f8bea63823409d76c6e9b1ccf7c106c3
Jeff Bell <jeff@jhbell.com>
2017-01-29 22:19:41 -0600

    Add classpath.sh

---
+++ P2/setclasspath.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+CLASSPATH=$PWD:
+CLASSPATH+=$PWD/lib/dist/lib/commons-collections.jar:
+CLASSPATH+=$PWD/lib/dist/lib/commons-io-2.4.jar:
+CLASSPATH+=$PWD/lib/dist/lib/commons-lang.jar:
+CLASSPATH+=$PWD/lib/dist/lib/junit-4.12.jar:
+CLASSPATH+=$PWD/lib/dist/lib/velocity.jar:
+CLASSPATH+=$PWD/lib/dist/lib/violet.jar:
+export CLASSPATH
+echo "done"
