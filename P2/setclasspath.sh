c781373efc5be01884f65beeb4ae66939bae178a
Jeff Bell <jeff@jhbell.com>
2017-01-29 22:23:48 -0600

    Add MDELite6.jar to classpath

--- P2/setclasspath.sh
+++ P2/setclasspath.sh
@@ -6,5 +6,6 @@ CLASSPATH+=$PWD/lib/dist/lib/commons-lang.jar:
 CLASSPATH+=$PWD/lib/dist/lib/junit-4.12.jar:
 CLASSPATH+=$PWD/lib/dist/lib/velocity.jar:
 CLASSPATH+=$PWD/lib/dist/lib/violet.jar:
+CLASSPATH+=$PWD/lib/dist/MDELite6.jar:
 export CLASSPATH
 echo "done"
