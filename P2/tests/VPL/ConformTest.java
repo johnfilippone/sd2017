af422415dc19f9842dd25df70d97a62d587d3377
John Filippone <johnpfilippone@yahoo.com>
2017-02-01 22:13:17 -0600

    Comments ConformTest

--- P2/tests/VPL/ConformTest.java
+++ P2/tests/VPL/ConformTest.java
@@ -1,56 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package VPL;
 
 import org.junit.Test;
 
 /**
  *
- * @author don
  */
 public class ConformTest {
     
     public ConformTest() {
     }
 
+    //don's tests
     @Test
     public void CarRental() {
         RegTest.Utility.redirectStdOut("out.txt");
         Conform.main("test-dependancies/validation-files/CarRental.vpl.pl");
         RegTest.Utility.validate("out.txt","Correct/CarRental.txt",false);
     }
-    
     @Test
     public void Comp() {
         RegTest.Utility.redirectStdOut("out.txt");
         Conform.main("test-dependancies/validation-files/Comp.vpl.pl");
         RegTest.Utility.validate("out.txt","Correct/Comp.txt",false);
     }
-    
     @Test
     public void DecayProduct() {
         RegTest.Utility.redirectStdOut("out.txt");
         Conform.main("test-dependancies/validation-files/DecayProduct.vpl.pl");
         RegTest.Utility.validate("out.txt","Correct/DecayProduct.txt",false);
     }
-    
     @Test
     public void diagram1() {
         RegTest.Utility.redirectStdOut("out.txt");
         Conform.main("test-dependancies/validation-files/diagram1.vpl.pl");
         RegTest.Utility.validate("out.txt","Correct/diagram1.txt",false);
     }
-    
     @Test
     public void squirrel() {
         RegTest.Utility.redirectStdOut("out.txt");
         Conform.main("test-dependancies/validation-files/squirrel.vpl.pl");
         RegTest.Utility.validate("out.txt","Correct/squirrel.txt",false);
     }
- 
     @Test
     public void straight() {
         RegTest.Utility.redirectStdOut("out.txt");
