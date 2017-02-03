4f0cd078d1a07739508efd96b5b80a92f71740ff
John Filippone <johnpfilippone@yahoo.com>
2017-02-03 11:36:30 -0600

    Adds test dependencies

--- P2/tests/test-dependencies/vpl/diamondBadCardinality.vpl.pl
+++ P2/tests/test-dependencies/vpl/diamondBadCardinality.vpl.pl
@@ -2,13 +2,12 @@
 dbase(vpl,[violetMiddleLabels,violetAssociation,violetInterface,violetClass]).
 
 table(violetClass,[id,"name","fields","methods",x,y]).
-violetClass(classnode0,'Class A','','',528,366).
-violetClass(classnode1,'Class B','','',896,370).
+violetClass(classnode0,'A','','',610,476).
+violetClass(classnode1,'B','','',901,478).
 
 table(violetInterface,[id,"name","methods",x,y]).
 
 table(violetAssociation,[id,"role1","arrow1",type1,"role2","arrow2",type2,"bentStyle","lineStyle",cid1,cid2]).
-violetAssociation(id0,'','',classnode,'','V',classnode,'HVH','',classnode1,classnode0).
+violetAssociation(id0,'','',classnode,'*','DIAMOND',classnode,'HVH','',classnode0,classnode1).
 
 table(violetMiddleLabels,[id,cid1,cid2,"label"]).
-violetMiddleLabels(id1,classnode1,classnode0,'Middle labels are bad').
