70d2f24a4238189b70d13fc3d436ddc49a2deeb9
Jeff Bell <jeff@jhbell.com>
2017-02-11 12:37:06 -0600

    Update run.sh include sample program outputs

--- P3/filippone-bell/run.sh
+++ P3/filippone-bell/run.sh
@@ -3,10 +3,38 @@ echo "setting classpath..."
 source setclasspath.sh
 echo "classpath set!"
 
-javac tests/TestToYuml.java
+echo "Sample outputs:"
+echo ""
+echo "./tests/pl/ERMetaModel.vpl.pl ==========================="
+java MDL.Vm2t ./tests/pl/ERMetaModel.vpl.pl src/toYuml.vm
+cat vm2toutput.txt
+echo ""
+echo "./tests/pl/gates.vpl.pl ================================="
+java MDL.Vm2t ./tests/pl/gates.vpl.pl src/toYuml.vm
+cat vm2toutput.txt
+echo ""
+echo "./tests/pl/hard.vpl.pl =================================="
+java MDL.Vm2t ./tests/pl/hard.vpl.pl src/toYuml.vm
+cat vm2toutput.txt
+echo ""
+echo "./tests/pl/school.vpl.pl ================================"
+java MDL.Vm2t ./tests/pl/shchool.vpl.pl src/toYuml.vm
+cat vm2toutput.txt
+echo ""
+echo "./tests/pl/shoppingCart.vpl.pl =========================="
+java MDL.Vm2t ./tests/pl/shoppingCart.vpl.pl src/toYuml.vm
+cat vm2toutput.txt
+echo ""
+echo "./tests/pl/University.vpl.pl ============================"
+java MDL.Vm2t ./tests/pl/University.vpl.pl src/toYuml.vm
+cat vm2toutput.txt
 
+rm vm2toutput.txt
+
+echo ""
 echo "Running tests..."
 cd tests
+javac TestToYuml.java
 java org.junit.runner.JUnitCore TestToYuml
 
 echo "Done."
