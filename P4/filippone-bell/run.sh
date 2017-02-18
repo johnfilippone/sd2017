2e77293b51e730e8d1a9db2db9dc9c72899379c2
Jeff Bell <jeff@jhbell.com>
2017-02-18 10:40:23 -0600

    Add run script

---
+++ P4/filippone-bell/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Run this from the filippone-bell directory
+echo "setting classpath..."
+source setclasspath.sh
+echo "classpath set!"
+
+echo ""
+echo "Compiling..."
+javac src/GatesApp/*.java
+javac src/logicGates/*.java
+javac src/Pins/*.java
+javac src/Errors/*.java
+javac test/GatesApp/*.java
+
+echo ""
+echo "Running tests..."
+java org.junit.runner.JUnitCore GatesApp.MainTest
+
+echo "Done."
