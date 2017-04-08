46e5ac5cd1ae29121ca2caad411ffb00c89ca53f
Jeff Bell <jeff@jhbell.com>
2017-04-08 00:03:08 -0500

    Unizp calc and remove netbeans garbage

---
+++ P8/filippone-bell/Calc/src/Application/Main.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Application;
+
+public class Main {
+    public static void main(String args[]) {
+        new D.Gui("DCalc").display();
+        //new BI.Gui("BICalc").display();
+    }
+}
