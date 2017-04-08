9ae4084c93dc391822688a88672a865ee91b4881
Jeff Bell <jeff@jhbell.com>
2017-04-08 00:12:59 -0500

    Move methods in D/Calc to match BI/Calc

--- P8/filippone-bell/Calc/src/D/Calc.java
+++ P8/filippone-bell/Calc/src/D/Calc.java
@@ -13,17 +13,12 @@ public class Calc {
     
     Double value = 0.0;
 
+    void sub(String n) { value = value - Double.parseDouble(n); }
+    void mul(String n) { value = value*Double.parseDouble(n); }
     void add(String n) { value = value+Double.parseDouble(n); }
-
+    void div(String n) { value = value/Double.parseDouble(n); }
+    String get() { return value.toString(); }
+    void set(String n) { value = Double.parseDouble(n); }
     void clear() { value =0.0; }
 
-    void div(String n) { value = value/Double.parseDouble(n); }
-
-    String get() { return value.toString(); }
-
-    void mul(String n) { value = value*Double.parseDouble(n); }
-
-    void set(String n) { value = Double.parseDouble(n); }
-
-    void sub(String n) { value = value - Double.parseDouble(n); }
 }
