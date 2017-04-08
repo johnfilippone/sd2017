28b34e088fc48d1bf9d82aa8ea4e9b546be7b51c
Jeff Bell <jeff@jhbell.com>
2017-04-08 00:22:41 -0500

    Create new packages and add sample files

---
+++ P8/filippone-bell/Calc/src/BIPlugIn/Calc.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package BIPlugIn;
+
+/**
+ *
+ * @author Don
+ */
+public class Calc {
+    
+    Double value = 0.0;
+
+    void sub(String n) { value = value - Double.parseDouble(n); }
+    void mul(String n) { value = value*Double.parseDouble(n); }
+    void add(String n) { value = value+Double.parseDouble(n); }
+    void div(String n) { value = value/Double.parseDouble(n); }
+    String get() { return value.toString(); }
+    void set(String n) { value = Double.parseDouble(n); }
+    void clear() { value =0.0; }
+
+}
