434d8b74790017ab8acd81fabe4d28821e2def68
John Filippone <johnpfilippone@yahoo.com>
2017-04-11 15:34:48 -0500

    Finishes P8

--- P8/filippone-bell/OOFrameworkShell/src/Application/Main.java
+++ P8/filippone-bell/OOFrameworkShell/src/Application/Main.java
@@ -5,8 +5,8 @@ public class Main {
     
     public static void main(String args[]) {
         Framework.Factory instance;
-        instance = new DPlugIn.Factory(); 
-        //instance = new BIPlugIn.Factory(); 
+        //instance = new DPlugIn.Factory(); 
+        instance = new BIPlugIn.Factory(); 
 
         instance.NewGui().display();
     }
