0683082c27dcfa70a00f65b94098df1e3993d4f2
Jeff Bell <jeff@jhbell.com>
2017-04-19 21:51:39 -0500

    Add ReadRelation.java

---
+++ P9/filippone-bell/gammaSupport/ReadRelation.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+/**
+  * @author John Filippone and Jeff Bell
+  */
+public class ReadRelation extends Thread {
+
+    BufferedReader in;
+    PrintStream out;
+
+    public Reader(String fileName, PrintStream out) {
+        try {
+            in = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+        } catch (Exception e) {
+            ReportError.msg(e.getMessage());
+        }
+        this.out = out;
+    }
+
+    public void run() {
+        try {
+            String input;
+            while (true) {
+                input = in.readLine();
+                if (input == null) {
+                    break;
+                }
+                out.println(input);
+                out.flush();
+            }
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+            ReportError.msg(this.getClass().getName() + e);
+        }
+    }
+}
