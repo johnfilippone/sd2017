4c5a5828fb8a5e8a0e8ba25678a0cc173814b92d
John Filippone <johnpfilippone@yahoo.com>
2017-05-01 21:55:13 -0500

    Starts HJoin

--- P9/filippone-bell/src/gamma/HJoin.java
+++ P9/filippone-bell/src/gamma/HJoin.java
@@ -8,26 +8,32 @@ import basicConnector.*;
 
 
 public class HJoin extends Thread {
-    Connector in;
-    Connector outArray[];
-    int joinKey;
+    Connector in1;
+    Connector in2;
+    Connector out;
+    int joinKey1;
+    int joinKey2;
 
-    public HJoin(Connector in, Connector[] outArray, int joinKey) {
-        this.in = in;
-        this.outArray = outArray;
-        this.joinKey = joinKey;
-        for(Connector connector : this.outArray)
-            connector.setRelation(this.in.getRelation());
+    public HJoin(Connector in1, Connector in2, Connector out, int joinKey1, int joinKey2) {
+        this.in1 = in1;
+        this.in2 = in2;
+        this.out= out;
+        this.joinKey1 = joinKey1;
+        this.joinKey2 = joinKey2;
+        out.setRelation(Relation.join(in1.getRelation(), in2.getRelation(), joinKey1, joinKey2));
         ThreadList.add(this);
     }
 
      public void run() {
         try {
-            Tuple t;
-            ReadEnd reader = in.getReadEnd();
+            Tuple t1;
+            Tuple t2;
+            ReadEnd reader1 = this.in1.getReadEnd();
+            ReadEnd reader2 = this.in2.getReadEnd();
             while (true) {
-                t = reader.getNextTuple();
-                if (t == null) {
+                t1 = reader1.getNextTuple();
+                t2 = reader2.getNextTuple();
+                if (t1 == null || t2 == null) {
                     break;
                 }
                 int hash = BMap.myhash(t.get(joinKey));
